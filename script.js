cf82ff23b4a9b95e4f4b64f406e9134c9ac24151
makssen <maks.sen.1903@mail.ru>
2022-05-27 14:20:40 +0300

    Object: add obj sum

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const arr = [1, 2, 3, 4, 5]
 const sum = arr.reduce((acc, item) => acc + item, 0)
 
 console.log(sum)
+
+const obj = {
+  a: 1,
+  b: 2
+}
+
+const sumObj = Object.values(obj).reduce((acc, item) => acc + item, 0)
+console.log(sumObj)
